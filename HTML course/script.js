baede4e0d8ddd7e63a3437dc822723b7a46c47e3
sv ko <dmitrybalance@mail.ru>
2023-01-26 20:26:44 +0300

    interval added

--- HTML course/script.js
+++ HTML course/script.js
@@ -1,5 +1,7 @@
-const person = {
-    name: 'Dima'
+const arr = []
+
+for (let i = 0; i < 10; i++) {
+    arr[i] = +((Math.random()*100)/10).toFixed(0)
 }
 
-console.log(typeof(person.name))
+console.log(arr)
