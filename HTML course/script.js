0a2ad469b7d7fcee37eb87b1f2fe9e107ffd0604
sv ko <dmitrybalance@mail.ru>
2023-02-06 17:00:10 +0300

    lines and rect

--- HTML course/script.js
+++ HTML course/script.js
@@ -1,11 +1,27 @@
-const set = new Set([1, 2, 2, 3, 4, 4, 5, 6, 6])
+const canvas = document.querySelector('#canvas')
 
-set.add(8).add(10)
-console.log(set.size)
+const ctx = canvas.getContext('2d')
 
-set.delete(1)
+ctx.fillStyle = 'rgb(200,0,0)'
+ctx.fillRect (50, 50, 50, 50)
 
-console.log(set)
+ctx.fillStyle = 'rgba(0, 100, 100, 0.5)'
+ctx.fillRect (70, 70, 50, 50)
 
-set.clear()
-console.log(set)
+ctx.fillStyle = 'rgb(100,100,100)'
+ctx.fillRect (90, 90, 50, 50)
+
+ctx.beginPath()
+ctx.moveTo(0, 50)
+ctx.lineTo(300, 50)
+ctx.strokeStyle = 'red'
+ctx.stroke()
+ctx.moveTo(0, 140)
+ctx.lineTo(300, 140)
+ctx.strokeStyle = 'grey'
+ctx.stroke()
+ctx.moveTo(120, 0)
+ctx.lineTo(120, 200)
+ctx.strokeStyle = 'rgba(0, 100, 100, 0.5)'
+ctx.stroke()
+ctx.endPath()
