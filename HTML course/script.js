e67fe076aeccf4b8c3ec2661f48f255b6e811132
sv ko <dmitrybalance@mail.ru>
2023-02-07 18:33:23 +0300

    complex arr

--- HTML course/script.js
+++ HTML course/script.js
@@ -1,27 +1,41 @@
-const canvas = document.querySelector('#canvas')
+// const canvas = document.querySelector('#canvas')
 
-const ctx = canvas.getContext('2d')
+// const ctx = canvas.getContext('2d')
 
-ctx.fillStyle = 'rgb(200,0,0)'
-ctx.fillRect (50, 50, 50, 50)
+// ctx.fillStyle = 'rgb(200,0,0)'
+// ctx.fillRect (50, 50, 50, 50)
 
-ctx.fillStyle = 'rgba(0, 100, 100, 0.5)'
-ctx.fillRect (70, 70, 50, 50)
+// ctx.fillStyle = 'rgba(0, 100, 100, 0.5)'
+// ctx.fillRect (70, 70, 50, 50)
 
-ctx.fillStyle = 'rgb(100,100,100)'
-ctx.fillRect (90, 90, 50, 50)
+// ctx.fillStyle = 'rgb(100,100,100)'
+// ctx.fillRect (90, 90, 50, 50)
 
-ctx.beginPath()
-ctx.moveTo(0, 50)
-ctx.lineTo(300, 50)
-ctx.strokeStyle = 'red'
-ctx.stroke()
-ctx.moveTo(0, 140)
-ctx.lineTo(300, 140)
-ctx.strokeStyle = 'grey'
-ctx.stroke()
-ctx.moveTo(120, 0)
-ctx.lineTo(120, 200)
-ctx.strokeStyle = 'rgba(0, 100, 100, 0.5)'
-ctx.stroke()
-ctx.endPath()
+// ctx.beginPath()
+// ctx.moveTo(0, 50)
+// ctx.lineTo(300, 50)
+// ctx.strokeStyle = 'red'
+// ctx.stroke()
+// ctx.moveTo(0, 140)
+// ctx.lineTo(300, 140)
+// ctx.strokeStyle = 'grey'
+// ctx.stroke()
+// ctx.moveTo(120, 0)
+// ctx.lineTo(120, 200)
+// ctx.strokeStyle = 'rgba(0, 100, 100, 0.5)'
+// ctx.stroke()
+// ctx.endPath()
+
+
+const arr = [
+    {
+        name: 'Dima',
+        age: [[2, 5, 
+        {
+            name: "alex",
+            age: 30
+        }], 2]
+    }
+]
+
+console.log(arr[0].age[0][2].age)
